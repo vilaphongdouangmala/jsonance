9547dddf1c8d9f793a134c94077fd9e599c46cd8
Vilaphong Douangmala <vilaphong.dml@gmail.com>
2025-10-19 12:23:19 +0700

    fixed lock file config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
